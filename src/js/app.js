4c55694708ac9041a504b92bce90763aba49d215
Fernando Díaz <feeldiac@gmail.com>
2022-02-24 21:45:01 -0500

    Created comics cards with real data. Created a general cards/card to syle

--- src/js/app.js
+++ src/js/app.js
@@ -2,32 +2,62 @@ import fetchData from './utils/fetchData.js';
 // 1dd16d0355176cfb236fb82b9bf1292eba2c6594346fae80605f6adf569c383e1977641f5
 // 696AE4745FE86EA2A721BB609F60A1EA
 
-const API = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=spider&limit=100&ts=1&apikey=46fae80605f6adf569c383e1977641f5&hash=696ae4745fe86ea2a721bb609f60a1ea';
+const spidersAPI = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=spider&limit=100&ts=1&apikey=46fae80605f6adf569c383e1977641f5&hash=696ae4745fe86ea2a721bb609f60a1ea';
+const comicsAPI = 'https://gateway.marvel.com:443/v1/public/comics?dateRange=2021-02-24%2C2022-02-24&titleStartsWith=spider&orderBy=-onsaleDate&limit=21&ts=1&apikey=46fae80605f6adf569c383e1977641f5&hash=696ae4745fe86ea2a721bb609f60a1ea';
 
-const anotherFunction = async (url_api) => {
+const spidersData = async(url_api) => {
     try {
         const data = await fetchData(url_api);
         const resultsArr = data.data.results;
         const container = document.querySelector('.spiders__cards');
+        const resultsCountText = document.querySelector('.spiders__results');
         let contentHTML = '';
 
         resultsArr.forEach(element => {
             contentHTML += `
-            <div class="spiders-card">
-                <div class="spiders-card__img">
-                    <a href="${element.urls[0].url}" target="_blank"></a>
+            <div class="spiders-card card">
+                <div class="spiders-card__img card__img">
+                    <a href="${element.urls[0].url}" target="_blank">
                         <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.name}">                                
                     </a>
                 </div>
-                    <span><a href="${element.urls[0].url}" target="_blank">${element.name}</a></span>
+                    <span><a href="${element.urls.length ? element.urls[0].url : 'http://marvel.com'}" target="_blank">${element.name}</a></span>
             </div>`
         });
 
         container.innerHTML = contentHTML;
+        resultsCountText.innerHTML = `Results: ${resultsArr.length} spiders`;
     
     } catch (error) {
         console.error(error);
     }
 }
 
-anotherFunction(API);
+const comicsData = async(url_api) => {
+    try {
+        const data =  await fetchData(url_api);
+        const resultsArr = data.data.results;
+        const container = document.querySelector('.comics__cards');
+        let contentHTML = '';
+
+        resultsArr.forEach(element => {
+            contentHTML += `
+            <div class="comics-card card">
+                <div class="comics-card__img card__img">
+                    <a href="${element.urls[0].url}" target="_blank">
+                        <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.title}">                                
+                    </a>
+                </div>
+                    <span><a href="${element.urls.length ? element.urls[0].url : 'http://marvel.com'}" target="_blank">${element.title}</a></span>
+            </div>`
+        });
+
+        container.innerHTML = contentHTML;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+spidersData(spidersAPI);
+comicsData(comicsAPI);
